85b5b08f94af70b1d5d43f94fe80ef82879ae0da
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-02-13 05:55:57 +0530

    added code

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,18 @@ provider "azurerm" {
   tenant_id       = var.ARM_TENANT_ID
 }
 
+# Define Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "Terraform-RG"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Terraform Backend Configuration
+terraform {
+  backend "azurerm" {
+    resource_group_name   = var.backend_resource_group
+    storage_account_name  = var.storage_account_name
+    container_name        = var.storage_container_name
+    key                   = var.state_file_name
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,30 @@ variable "ARM_CLIENT_ID" {}
 variable "ARM_CLIENT_SECRET" {}
 variable "ARM_SUBSCRIPTION_ID" {}
 variable "ARM_TENANT_ID" {}
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  default     = "Terraform-RG"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be deployed"
+}
+
+variable "backend_resource_group" {
+  description = "The resource group where Terraform state storage is located"
+}
+
+variable "storage_account_name" {
+  description = "The name of the Azure Storage Account for Terraform state"
+  default     = "your-storage-account-name"
+}
+
+variable "storage_container_name" {
+  description = "The name of the container for storing Terraform state files"
+  default     = "tfstate"
+}
+
+variable "state_file_name" {
+  description = "The name of the Terraform state file"
+  default     = "terraform.tfstate"
+}
